a773df42ae6fe1ea395be226e82d4deb6bdf178e
leeharyung <lhr4884@naver.com>
2023-12-06 02:26:17 +0900

    fix: today 페이지 수정

--- src/app/today/page.tsx
+++ src/app/today/page.tsx
@@ -1,97 +1,82 @@
-"use client";
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import { FaMicrophone } from "react-icons/fa6";
-import { SiGooglemessages } from "react-icons/si";
+'use client';
+
+import React, { Fragment, useState } from 'react';
 import { IoPaperPlane } from 'react-icons/io5';
 
-interface ResponseData {
-    user_statement: string;
-  }
+import { postTodo } from '@/app/apis/todo';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import { Todo } from '@/types';
 
-const TodayPage: React.FC = () => {
-    const [inputText, setInputText] = useState<string>("");
-    const [response, setResponse] = useState<ResponseData | null>(null);
-    const [error, setError] = useState<string | null>(null);
+const TodayPage = () => {
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputText(e.target.value);
-    };
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
 
-    const handleButtonClick = async () => {
-        try {
-            // HTTP POST 요청을 보낼 URL
-            const apiUrl = 'http://ai.na2ru2.me/v1/openai';
-      
-            // 요청 데이터
-            const requestData = {
-              user_statement: inputText,
-            };
-      
-            // HTTP POST 요청 보내기
-            const response = await fetch(apiUrl, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(requestData),
-            });
-      
-            // HTTP 응답 확인
-            if (!response.ok) {
-              throw new Error('서버 응답에 실패했습니다.');
-            }
-      
-            // JSON 형식으로 응답 데이터 파싱
-            const responseData: ResponseData = await response.json();
-      
-            // 응답 데이터를 상태에 저장
-            setResponse(responseData);
-          } catch (error) {
-            // 오류 발생 시 오류 상태에 저장
-            setError(error.message);
-          }
-          console.log(response);
-          setInputText("");
-    };
+  const handleKeyUpInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleButtonClick();
+    }
+  };
 
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <Header />
-            <div className='flex h-96'>
+  const handleButtonClick = async () => {
+    if (inputText === '') return;
 
-                <div className='bg-white p-4 w-72 h-16 pt-3 flex mt-80 rounded-t-lg'>
-                    <div className="relative flex items-center w-full">
-                        <button className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-transparent border-none">
-                            <SiGooglemessages 
-                                className='text-[#848388]'
-                            />
-                        </button>
-                        <input
-                            type="text"
-                            placeholder="이렇게 입력해보세요"
-                            className="w-full h-7 pl-7 pb-1 rounded-md border border-gray-300 focus:outline-none focus:ring-2 placeholder:text-[0.7rem] bg-[#efeef1]"
-                            value={inputText}
-                            onChange={handleInputChange}
-                        />
-                        <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-transparent border-none">
-                            <FaMicrophone
-                                className='text-[#848388]'
-                            />
-                        </button>
-                    </div>
-                    <button className="ml-4 h-6 mt-1 w-20 bg-[#78be5e] text-white px-4 rounded-2xl text-xs pr-0 mt-[5.5px] hover:bg-lime-400" onClick={handleButtonClick}>
-                        <IoPaperPlane 
-                            className='absolute ml-[-6px] mt-0.5'
-                        />
-                        전송
-                    </button>
-                </div>
-            </div>
-            <Footer />
-        </div>
-    );
+    postTodo(inputText)
+      .then((data) => {
+        setTodos((prev) => [...prev, data]);
+
+        setInputText('');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <Fragment>
+      <Header />
+      <span className="text-sm text-gray-500 p-2">오늘의 작업들</span>
+      <div className="flex flex-col flex-1 overflow-auto">
+        <ul className="border-b">
+          {Array.from({ length: 20 }).map((_, i) => (
+            <li
+              key={i}
+              className="p-2 flex items-center gap-2 bg-white border-t border-gray-200"
+            >
+              <button className="w-4 h-4 rounded-full border-2 border-gray-400" />
+              <div className="flex flex-col">
+                <div className="text-gray-700">캡스톤 디자인 과제 끝내기</div>
+                <div className="text-sm text-gray-400">2021-09-01</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="bg-white w-full flex gap-2 px-2 py-3 rounded-t-lg">
+        <input
+          type="text"
+          placeholder="이렇게 입력해보세요"
+          className="w-full pl-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 bg-[#efeef1]"
+          value={inputText}
+          onChange={handleChangeInput}
+          onKeyUp={handleKeyUpInput}
+        />
+        <button
+          className="bg-[#78be5e] flex gap-1 justify-center whitespace-nowrap p-2 items-center text-white rounded-xl hover:bg-lime-400"
+          onClick={handleButtonClick}
+        >
+          <IoPaperPlane />
+          전송
+        </button>
+      </div>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default TodayPage;
