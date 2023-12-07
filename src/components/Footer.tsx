b764b12fb00e17029f4dbebdc4e6d787a60399e9
halang <lhr4884@naver.com>
2023-12-07 21:00:03 +0900

    feature: 전체 UI 수정 및 api 연동 (#1)
    * feat: prettierrc 설정
    
    * feat: gitignore 추가
    
    * chore: 필요한 라이브러리 추가 설치
    
    * chore: 필요한 라이브러리 추가 설치
    
    * fix: 폴더구조 변경 및 UI 수정
    
    * fix: globalcss 수정
    
    * fix: tailwind config 수정
    
    * feat: api 추가
    
    * feat: type 추가
    
    * fix: 전체 레이아웃 수정
    
    * fix: today 페이지 수정

---
+++ src/components/Footer.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+
+const Footer = () => {
+  return (
+    <footer className="border-t flex justify-around items-center w-full p-4">
+      <Image
+        src="assets/icons/inspiration.svg"
+        alt="임시 이미지"
+        width="25"
+        height="25"
+      />
+      <Image
+        src="assets/icons/inspiration.svg"
+        alt="임시 이미지"
+        width="25"
+        height="25"
+      />
+      <Image
+        src="assets/icons/inspiration.svg"
+        alt="임시 이미지"
+        width="25"
+        height="25"
+      />
+      <Image
+        src="assets/icons/inspiration.svg"
+        alt="임시 이미지"
+        width="25"
+        height="25"
+      />
+    </footer>
+  );
+};
+
+export default Footer;
