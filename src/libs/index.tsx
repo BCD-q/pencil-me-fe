700a7305655681ad046595ff8af8e8f621a6388a
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-03-26 21:03:39 +0900

    feat: 관심사 페이지 마크업

--- src/libs/index.tsx
+++ src/libs/index.tsx
@@ -1,16 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export const GroupQuery = () => {
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['group'],
-    queryFn: async () => {
-      return axios.get('http://capstone.na2ru2.me/api/v1/categories');
-    },
-  });
+export const fetchTodo = async () => {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error</div>;
-
-  return <></>;
+  await axios.patch(`${apiKey}language`);
 };
