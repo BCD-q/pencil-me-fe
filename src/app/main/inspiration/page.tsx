1eac38093070933dda30b73b781681196bc4bfdc
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-04-02 16:37:00 +0900

    feat: 바텀 캐러셀 수정

--- src/app/main/inspiration/page.tsx
+++ src/app/main/inspiration/page.tsx
@@ -6,14 +6,14 @@ import Carousel from './components/Carousel';
 
 export default function InspirationPage(): JSX.Element {
   return (
-    <div className="h-full">
+    <div className="h-full flex flex-col">
       <Cartegory>영감</Cartegory>
       <WorkBar>오늘의 제안</WorkBar>
       <div className="bg-white h-48">
         <Carousel />
       </div>
       <WorkBar>이런건 어때요?</WorkBar>
-      <div className="bg-white h-1/3">
+      <div className="bg-white flex-1">
         <BottonInspiration />
       </div>
     </div>
