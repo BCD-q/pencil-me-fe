6ca6d804cbe1c8d08f4c600c213814faa981fa53
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-04-07 20:53:47 +0900

    feat: 영감 api 연결 테스트 (#19)

--- src/app/main/inspiration/[id]/page.tsx
+++ src/app/main/inspiration/[id]/page.tsx
@@ -4,9 +4,9 @@ export default function Detail() {
   return (
     <div className="w-full">
       <Cartegory>디테일</Cartegory>
-      <main className="flex flex-col h-1/3">
-        <div className="flex justify-center">detail Name</div>
-        <div className="w-3/4 mx-auto">
+      <main className="flex flex-col h-2/3 w-3/4 mx-auto">
+        <div className="flex mx-auto box-border">detail Name</div>
+        <div className="">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium
           nulla necessitatibus ad, ullam tempora nobis laborum qui reprehenderit
           iste aperiam, tenetur dignissimos eligendi rerum, possimus impedit
