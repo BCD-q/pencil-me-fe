3ad059e902ac224c604bb506cd3855a1b7801656
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-04-08 12:42:59 +0900

    feat: 동기부여 페이지 모의 api 연동

--- src/app/main/inspiration/components/BottonComponent.tsx
+++ src/app/main/inspiration/components/BottonComponent.tsx
@@ -7,7 +7,7 @@ export default function BottomCarousel({ data }: { data: any }) {
     <div className="h-fit">
       <main className="flex my-2">
         <div className="rounded-xl h-1/2 w-36 xs:w-36 sm:w-44 shadow-xl mx-2 flex-col border-2">
-          <Image src={data.message} alt="" className="" />
+          <img src={data} alt="" className="w-36 h-36" />
           <div className="bg-gray-100 flex h-1/4">
             <div className="flex-col w-2/3 whitespace-nowrap overflow-hidden text-ellipsis mx-2">
               <div className="text-xs w-20 ml-1">서버 기술 블로그</div>
