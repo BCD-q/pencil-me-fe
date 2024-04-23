fdebed3ad04d0edfb19c5ce0deaf358db740c5ec
halang <lhr4884@naver.com>
2024-04-23 22:43:14 +0900

    feat: 로그인 연동 및 회원가입 페이지 수정 (#27)
    * fix: 로그인 페이지 api 연동
    
    * fix: 회원가입 페이지 수정

--- src/app/login/components/RegisterInput.tsx
+++ src/app/login/components/RegisterInput.tsx
@@ -1,61 +1,52 @@
 'use client';
 
-import Link from 'next/link';
-import { useCallback, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 
 import { Info } from '@/libs/useRegister';
 
-export default function RegisterInput(): JSX.Element {
-  const [inputs, setInputs] = useState({
-    uid: '',
-    password1: '',
-    password2: '',
-    email: '',
-    nickname: '',
-  });
+const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
+export default function RegisterInput(): JSX.Element {
+  const router = useRouter();
+  const [uid, setUid] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
   const [submit, setSubmit] = useState(false);
 
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const mutation = useMutation({
+    mutationFn: (Info: Info) => {
+      return axios.post(`${apiKey}/members`, Info);
+    },
+    onSuccess: () => {
+      alert('회원가입이 완료되었습니다!');
+      router.push('/login');
+    },
+    onError: () => {
+      alert('회원가입에 실패했습니다!');
+    },
+  });
 
   // 비밀번호 중복 확인
   const IsPassword = () => {
-    const { password1, password2 } = inputs;
-    if (password2 == '') return;
-    else if (password1 === password2) {
+    if (confirmPassword == '') return;
+    else if (password === confirmPassword) {
       return (
         <span className="mt-2 text-[#78be5e] mx-auto">
           비밀번호가 일치합니다!
         </span>
       );
-    } else if (password1 !== password2)
+    } else if (password !== confirmPassword)
       return (
         <span className="mt-2 text-red-600 mx-auto">
           비밀번호가 일치하지 않습니다. 다시 입력해주세요
         </span>
       );
-  };
-
-  const IsRegister = ({ Info }: { Info: Info }) => {
-    const mutation = useMutation({
-      mutationFn: (Info: Info) => {
-        return axios.post(`${apiKey}members`, Info);
-      },
-    });
-
-    return (
-      <button
-        className="btn bg-[#78be5e] mt-10 mb-6 text-white w-11/12 mx-auto text-lg"
-        onClick={() => {
-          mutation.mutate(Info);
-        }}
-      >
-        회원가입
-      </button>
-    );
   };
 
   const IsValid = (): void => {
@@ -68,9 +59,10 @@ export default function RegisterInput(): JSX.Element {
       <div className="flex flex-row">
         <input
           type="text"
-          placeholder="계정명"
+          placeholder="아이디"
+          value={uid}
           className="w-7/12 mx-auto mt-1 rounded-lg input input-bordered"
-          onChange={(e) => setInputs({ ...inputs, uid: e.target.value })}
+          onChange={(e) => setUid(e.target.value)}
         />
         <button className="btn w-3/12 px-0 my-auto mx-auto bg-[#78be5e] text-white text-lg rounded-2xl">
           중복 확인
@@ -79,36 +71,46 @@ export default function RegisterInput(): JSX.Element {
       <input
         type="text"
         placeholder="이메일"
+        value={email}
         className="w-11/12 mx-auto mt-2 rounded-lg input input-bordered"
-        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="text"
         placeholder="닉네임"
+        value={nickname}
         className="w-11/12 mx-auto mt-2 rounded-lg input input-bordered"
-        onChange={(e) => setInputs({ ...inputs, nickname: e.target.value })}
+        onChange={(e) => setNickname(e.target.value)}
       />
       <input
         type="password"
         placeholder="비밀번호"
+        value={password}
         className="w-11/12 mx-auto mt-2 rounded-lg input input-bordered"
-        onChange={(e) => setInputs({ ...inputs, password1: e.target.value })}
+        onChange={(e) => setPassword(e.target.value)}
       />
       <input
         type="password"
         placeholder="비밀번호 재입력"
+        value={confirmPassword}
         className="w-11/12 mx-auto mt-2 rounded-lg input input-bordered"
-        onChange={(e) => setInputs({ ...inputs, password2: e.target.value })}
+        onChange={(e) => setConfirmPassword(e.target.value)}
       />
       {IsPassword()}
-      {/* <Link
-        href="../main"
+
+      <button
         className="btn bg-[#78be5e] mt-10 mb-6 text-white w-11/12 mx-auto text-lg"
-        onClick={IsRegister}
+        onClick={() => {
+          mutation.mutate({
+            uid,
+            password,
+            email,
+            nickname,
+          });
+        }}
       >
-        <button>회원가입</button>
-      </Link> */}
-      <IsRegister Info={inputs} />
+        회원가입
+      </button>
     </>
   );
 }
