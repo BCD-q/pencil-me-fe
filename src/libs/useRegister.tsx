5aa1dbc42a3a6de86dc2f2afec9458ea4e4541c1
leeharyung <lhr4884@naver.com>
2024-04-23 22:42:29 +0900

    fix: 회원가입 페이지 수정

--- src/libs/useRegister.tsx
+++ src/libs/useRegister.tsx
@@ -5,8 +5,7 @@ const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export interface Info {
   uid: string;
-  password1: string;
-  password2: string;
+  password: string;
   email: string;
   nickname: string;
 }
