5fe69bd19ef17ef813ec75f12f3bd966b76a9a07
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-04-25 12:21:33 +0900

    feat: 그룹 페이지 api 호출 코드 변경

--- src/app/group/components/GroupBox.tsx
+++ src/app/group/components/GroupBox.tsx
@@ -1,39 +1,32 @@
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
+'use client';
 
-import useGroupStore from '@/modules/groupStore';
+import { useQuery } from '@tanstack/react-query';
+
+import { fetchCategory } from '@/libs';
 
 interface category {
-  categoryId: number;
-  memberId: number;
-  categoryName: string;
+  categoryId?: number;
+  memberId?: number;
+  categoryName?: string;
 }
 
-export default function GroupBox(): JSX.Element {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const [category, setCategory] = useState<category[]>();
-  const { groupModalOpen } = useGroupStore();
+export default function GroupDataBox() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['category'],
+    queryFn: () => fetchCategory(),
+  });
 
-  const viewCategory = async () => {
-    const res = await fetch(`${apiKey}categories`, {
-      method: 'GET',
-    });
-    const data = await res.json();
-    setCategory(data.data);
-  };
-
-  useEffect(() => {
-    viewCategory();
-  }, [groupModalOpen]);
+  if (isLoading)
+    return <div className="flex loading loading-spinner w-2/5 mx-auto"></div>;
 
   return (
     <div className="flex flex-col items-center ">
-      {category &&
-        category.map((item, index) => (
+      {data &&
+        data.data.map((item: category, index) => (
           <button
             className={`flex justify-start w-11/12 h-12 pt-2 pl-4 text-lg border-b-[1px] bg-white rounded-sm ${
               index === 0 ? 'rounded-t-lg' : ''
-            } ${index === category.length - 1 ? 'rounded-b-lg ' : ''}`}
+            } ${index === data.data.length - 1 ? 'rounded-b-lg ' : ''}`}
             key={index}
           >
             {item.categoryName}
--- src/app/login/components/LoginInput.tsx
+++ src/app/login/components/LoginInput.tsx
@@ -19,7 +19,8 @@ export default function LoginInput() {
     },
     onSuccess: ({ data }) => {
       console.log(data);
-      localStorage.setItem('token', data.token);
+      localStorage.setItem('token', data.data.token);
+      axios.defaults.headers.common['Authorization'] = data.data.token;
       alert('로그인이 완료되었습니다!');
       router.push('/main');
     },
