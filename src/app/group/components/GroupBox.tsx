e855027b473eed825bd5c3c6bdcc03f5280a2268
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-05-02 11:31:08 +0900

    feat: input창 focus 확대 방지, 그룹 수정 ,삭제 api 연동 (#32)
     그룹 수정 ,삭제 api 연동 => 아직 완료되지않음

--- src/app/group/components/GroupBox.tsx
+++ src/app/group/components/GroupBox.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from 'react';
 
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { fetchCategory } from '@/libs';
 
 import useGroupStore from '../../../modules/groupStore';
+import ModifyGroupModal from './ModifyGroupModal';
 
 interface category {
   categoryId?: number;
@@ -12,8 +16,29 @@ interface category {
   categoryName?: string;
 }
 
+const testData = [
+  {
+    categoryId: 1,
+    categoryName: 'category1',
+  },
+  {
+    categoryId: 2,
+    categoryName: 'category2',
+  },
+  {
+    categoryId: 3,
+    categoryName: 'category3',
+  },
+];
+
 export default function GroupDataBox() {
-  const { groupModalOpen, setGroupModalClose } = useGroupStore();
+  const {
+    groupModalOpen,
+    setGroupModalClose,
+    modModalOpen,
+    setModGroupOpen,
+    setModGroupClose,
+  } = useGroupStore();
 
   const { data, isLoading, error, refetch } = useQuery({
     queryKey: ['category'],
@@ -22,26 +47,58 @@ export default function GroupDataBox() {
 
   useEffect(() => {
     refetch();
-  }, [groupModalOpen]);
+  }, [groupModalOpen, modModalOpen]);
 
   if (isLoading)
     return <div className="flex loading loading-spinner w-2/5 mx-auto"></div>;
 
   return (
-    <ul className="h-full w-full">
-      {data?.data?.data.map((item: category, index: number) => {
+    // <ul className="h-full w-full">
+    //   {data?.data?.data.map((item: category, index: number) => {
+    //     const isFirst = index === 0;
+    //     const isLast = index === data.data.data.length - 1;
+    //     const buttonClassName = `
+    //       flex mx-auto w-11/12 h-12 pt-2 pl-4 text-lg border-b-[1px] bg-white
+    //       ${isFirst ? 'rounded-t-lg' : ''} ${isLast ? 'rounded-b-lg' : ''}
+    //     `;
+    //     return (
+    //       <button className={buttonClassName} key={index}>
+    //         {item.categoryName}
+    //       </button>
+    //     );
+    //   })}
+    // </ul>
+    <ul>
+      {testData.map((item: category, index: number) => {
         const isFirst = index === 0;
-        const isLast = index === data.data.data.length - 1;
+        const isLast = index === testData.length - 1;
         const buttonClassName = `
-          flex mx-auto w-11/12 h-12 pt-2 pl-4 text-lg border-b-[1px] bg-white
+          flex mx-auto w-11/12 h-12 items-center pl-4 text-lg border-b-[1px] bg-white
           ${isFirst ? 'rounded-t-lg' : ''} ${isLast ? 'rounded-b-lg' : ''}
         `;
         return (
-          <button className={buttonClassName} key={index}>
-            {item.categoryName}
-          </button>
+          <Swiper key={index}>
+            <SwiperSlide key={index}>
+              <button className={buttonClassName}>{item.categoryName}</button>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="flex items-center w-11/12 mx-auto bg-white">
+                <div className="flex flex-row w-full h-12 text-sm text-white border-t">
+                  <div className="flex-1"></div>
+                  <button
+                    className="w-1/6 bg-orange-400"
+                    onClick={setModGroupOpen}
+                  >
+                    수정
+                  </button>
+                  <button className="w-1/6 bg-red-500">삭제</button>
+                </div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
         );
       })}
+      {modModalOpen && <ModifyGroupModal />}
     </ul>
   );
 }
---
+++ src/app/group/components/ModifyGroupModal.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from 'react';
+
+import useGroupStore from '../../../modules/groupStore';
+
+export type categoryProps = {
+  categoryName: string;
+};
+
+export default function AddGroupModal(): JSX.Element {
+  const { modModalOpen, setModGroupClose } = useGroupStore();
+
+  const [groupName, setGroupName] = useState<categoryProps | undefined>();
+  const modalBackground = useRef<HTMLDivElement>(null);
+
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  const setGroup = async () => {
+    if (groupName) {
+      console.log(groupName.categoryName);
+      await fetch(`${apiKey}/categories`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token') as string,
+        },
+        body: JSON.stringify({
+          name: groupName.categoryName,
+        }),
+      });
+      setModGroupClose();
+    }
+  };
+
+  const handleKeyUpInput = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setGroup();
+    }
+  };
+
+  return (
+    <div
+      className="absolute z-10 flex flex-col w-4/5  h-48 -translate-x-1/2 -translate-y-1/2 bg-white shadow-xl top-1/2 left-1/2 rounded-xl md:w-2/5 md:h-52 opacity-100"
+      ref={modalBackground}
+    >
+      <div className="flex justify-center my-4">그룹명</div>
+      <input
+        type="text"
+        placeholder="그룹명을 입력해주세요"
+        className="flex w-10/12 mx-auto text-center rounded-lg just-center input input-bordered"
+        onChange={(e) => setGroupName({ categoryName: e.target.value })}
+        onKeyUp={handleKeyUpInput}
+      />
+      <div className="flex justify-evenly flex-1 items-center">
+        <button
+          className="btn rounded-lg bg-accent text-white"
+          onClick={setGroup}
+        >
+          등록
+        </button>
+        <button
+          className="btn rounded-lg bg-accent text-white"
+          onClick={setModGroupClose}
+        >
+          닫기
+        </button>
+      </div>
+    </div>
+  );
+}
