4975a85f7920876e023d83bc180c53e12cfc5e26
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-05-16 15:16:36 +0900

    feat: next.config 수정

--- next.config.js
+++ next.config.js
@@ -6,18 +6,6 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/members/sign-in',
-        destination: 'https://na2ru2.me:6378/api/v1/members/sign-in',
-      },
-      {
-        source: '/api/v1/categories',
-        destination: 'https://na2ru2.me:6378/api/v1/categories',
-      },
-    ];
-  },
 };
 
 module.exports = withPWA(nextConfig);
