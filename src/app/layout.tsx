154cc788ba5ac82d7e2e064ac41d09a70ab2fd58
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-05-16 10:54:27 +0900

    feat: 스플래시 이미지 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,67 +22,58 @@ export const metadata: Metadata = {
   description:
     '사용자의 일상과 관심사를 중심으로 인공지능을 통해 개인의 생산성을 극대화하는 다양한 기능을 포함한 애플리케이션',
   manifest: '/manifest.json',
-  icons: {
-    other: [
+  appleWebApp: {
+    startupImage: [
+      '/icons/splashscreens/iphone5_splash.png',
       {
         url: '/icons/splashscreens/iphone5_splash.png',
         media:
           '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/iphone6_splash.png',
         media:
           '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/iphoneplus_splash.png',
         media:
           '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/iphonex_splash.png',
         media:
           '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/iphonexr_splash.png',
         media:
           '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/iphonexsmax_splash.png',
         media:
           '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/ipad_splash.png',
         media:
           '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/ipadpro1_splash.png',
         media:
           '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/ipadpro3_splash.png',
         media:
           '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
       {
         url: '/icons/splashscreens/ipadpro2_splash.png',
         media:
           '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)',
-        rel: '/apple-touch-startup-image',
       },
     ],
   },
