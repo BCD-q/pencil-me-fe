4156d541e233d77fc60f385048352ad3027725fe
Jungwoo Hong <ghost1434@inu.ac.kr>
2024-05-20 15:11:39 +0900

    feat: 로그아웃 버튼 UI 변경

--- src/app/main/profile/components/LogOut.tsx
+++ src/app/main/profile/components/LogOut.tsx
@@ -7,7 +7,7 @@ export default function LogOut() {
 
   return (
     <div
-      className="flex sticky border-none h-[7%] top-auto mb-2 bg-accent rounded-xl w-1/4 mx-auto justify-center items-center"
+      className="flex sticky border-none h-[5%] top-auto mb-2 bg-accent rounded-xl w-1/4 mx-auto justify-center items-center"
       onClick={() => {
         router.push('/login');
       }}
--- src/app/main/profile/page.tsx
+++ src/app/main/profile/page.tsx
@@ -5,9 +5,9 @@ import LogOut from './components/LogOut';
 
 export default function ProfilePage(): JSX.Element {
   return (
-    <div className="flex flex-col h-[70vh]">
+    <div className="flex flex-col h-[100vh]">
       <Cartegory>프로필</Cartegory>
-      <div className="flex flex-col items-center h-[100vh] bg-gray-200 border-8 border-slate-200 ">
+      <div className="flex flex-col items-center h-[70vh] bg-gray-200 border-8 border-slate-200 ">
         <AvatarBox />
       </div>
       <LogOut />
